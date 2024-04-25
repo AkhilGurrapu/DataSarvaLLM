d14b4dfb907e16594b5820cb41ba8991d9603543
Akhil Gurrapu <akhilgur123@gmail.com>
2024-04-25 12:55:00 -0500

    updated

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import streamlit as st
-
 from openai import OpenAI
-# from dotenv import load_dotenv
-# import os
+from dotenv import load_dotenv
+import os
 
 # Load environment variables
-# load_dotenv()
-# api_key = os.getenv("OPENAI_API_KEY")
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 # Initialize the OpenAI client
 client = OpenAI(
     base_url="https://integrate.api.nvidia.com/v1",
-    api_key= "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"
+    api_key=api_key
 )
 
 def generate_response(model, prompt, temperature, max_tokens):
@@ -21,7 +20,7 @@ def generate_response(model, prompt, temperature, max_tokens):
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=1,
-        stream=True
+        stream=False
     )
     return completion.choices[0].message.content
 
@@ -29,10 +28,11 @@ def generate_response(model, prompt, temperature, max_tokens):
 st.set_page_config(page_title="DataSarva", layout="wide")
 
 # Custom CSS styling
-st.markdown("""
+st.markdown(
+    """
     <style>
         .stTextArea label, .stSlider label {
-            font-size: 10px;
+            font-size: 18px;
             font-weight: bold;
         }
         .stTextArea textarea, .stSlider .widget {
@@ -52,7 +52,9 @@ st.markdown("""
             background-color: #0056b3;
         }
     </style>
-""", unsafe_allow_html=True)
+    """,
+    unsafe_allow_html=True
+)
 
 # Header
 col1, col2, col3 = st.columns([2, 2, 1])
@@ -60,17 +62,15 @@ with col3:
     # st.image("DataSarva.png", width=100)
     st.title("DataSarva")
     st.write("Developed by Akhil Gurrapu")
-    
 
 # Divide the page into columns
 col4, col5 = st.columns([2, 3])
 
 with col4:
-    st.subheader('Chat with below    LLM models')
+    st.subheader('Chat with below LLM models')
     # List of available models
     model_names = ["Snowflake-Arctic", "Meta-Llama3-70B", "Meta-Llama3-8B", "Mistral-8x22B", "Google-CodeGemma-7B", "Google-Gemma-7B"]
     model_values = ["snowflake/arctic", "meta/llama3-70b-instruct", "meta/llama3-8b-instruct", "mistralai/mixtral-8x22b-instruct-v0.1", "google/codegemma-7b", "google/gemma-7b"]
-
     # Filter buttons for model selection
     selected_model_value = st.radio("Select a model", model_values, index=1, format_func=lambda x: model_names[model_values.index(x)])
 
@@ -85,9 +85,7 @@ with col5:
             max_tokens = st.slider("Max Tokens", 100, 1500, 1024, step=10, label_visibility="visible")
         submit_button = st.form_submit_button(label='Submit', use_container_width=True)
 
-if submit_button and prompt:
-    with st.spinner("Generating response..."):
-        response = generate_response(selected_model_value, prompt, temperature, max_tokens)
-    st.success(response)
-
-st.markdown(f'<a href="https://linkedin.com/in/akhilgurrapu" target="_blank">My LinkedIn Profile</a>', unsafe_allow_html=True)
+        if submit_button and prompt:
+            with st.spinner("Generating response..."):
+                response = generate_response(selected_model_value, prompt, temperature, max_tokens)
+            st.success(response)
